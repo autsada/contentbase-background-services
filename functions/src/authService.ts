ba177322bc4443f3f981db5ebfdd1b530732673b
Autsada T <autsada@gmail.com>
2023-01-13 11:51:56 +0700

    update auth background fn logic

--- functions/src/authService.ts
+++ functions/src/authService.ts
@@ -10,17 +10,41 @@ export const onUserCreated = functions
   .runWith({
     minInstances,
     maxInstances: 100,
-    secrets: ["SUPER_ADMIN_EMAIL"],
+    secrets: ["SUPER_ADMIN_EMAIL", "ADMIN_EMAILS"],
   })
   .auth.user()
   .onCreate(async (user) => {
     const uid = user.uid
     const email = user.email?.toLowerCase()
+    const providerId = user.providerData[0].providerId
+
     try {
-      if (email === process.env.SUPER_ADMIN_EMAIL?.toLowerCase()) {
-        // Super admin
-        await auth.setCustomUserClaims(uid, { super_admin: true })
+      if (providerId === "password") {
+        if (email) {
+          const superAdminEmail = process.env.SUPER_ADMIN_EMAIL?.toLowerCase()
+          let adminEmails: string[] =
+            JSON.parse(process.env.ADMIN_EMAILS! || "") || []
+
+          if (adminEmails.length > 0) {
+            adminEmails = adminEmails.map((e: string) => e.toLowerCase())
+          }
+
+          if (email === superAdminEmail) {
+            // Super admin
+            await auth.setCustomUserClaims(uid, { super_admin: true })
+          } else if (adminEmails.includes(email)) {
+            // Admin
+            await auth.setCustomUserClaims(uid, { admin: true })
+          } else {
+            // Client
+            await auth.setCustomUserClaims(uid, { client: true })
+          }
+        } else {
+          // Client
+          await auth.setCustomUserClaims(uid, { client: true })
+        }
       } else {
+        // Client
         await auth.setCustomUserClaims(uid, { client: true })
       }
 
